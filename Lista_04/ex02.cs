2b632894d686a7cb746dc984aed310b1abedbe6f
Victor Silva <110117696+victordsilva1@users.noreply.github.com>
2024-05-20 14:58:55 -0300

    Update ex02.cs

--- Lista_04/ex02.cs
+++ Lista_04/ex02.cs
@@ -1,1 +1,42 @@
+class Program
+{
+    static void Main()
+    {
 
+        Calchab();
+
+    }
+
+    static void Calchab()
+    {
+        double somaSalarios = 0;
+        int contador = 0;
+
+        while (true)
+        {
+            Console.Write("Digite o salário (ou um valor negativo para sair): ");
+            double salario = Convert.ToDouble(Console.ReadLine());
+
+            if (salario < 0)
+            {
+                if (contador == 0)
+                {
+                    Console.WriteLine("Nenhum dado foi inserido.");
+                }
+                else
+                {
+                    double mediaSalario = somaSalarios / contador;
+                    Console.WriteLine($"A média de salário da população é: {mediaSalario:C}");
+                }
+                break;
+            }
+
+            Console.Write("Digite o número de filhos: ");
+            int numeroFilhos = Convert.ToInt32(Console.ReadLine());
+
+            somaSalarios += salario;
+            contador++;
+        
+        }
+    }
+}
