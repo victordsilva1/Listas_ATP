152ff84ca9d1f47dd4dc048e4b44f5bc5b97aee7
Victor Silva <110117696+victordsilva1@users.noreply.github.com>
2024-07-01 15:21:27 -0300

    Update ex01.cs

--- ATP/lista06/ex01.cs
+++ ATP/lista06/ex01.cs
@@ -1,1 +1,25 @@
+using System;
 
+class ContarEspacos
+{
+    static void Main()
+    {
+        // Solicita ao usuário que insira uma frase
+        Console.WriteLine("Digite uma frase:");
+        string frase = Console.ReadLine();
+
+        int contagemEspacos = 0;
+
+        // Conta espaços em branco na frase
+        foreach (char c in frase)
+        {
+            if (char.IsWhiteSpace(c))
+            {
+                contagemEspacos++;
+            }
+        }
+
+        // Exibe a quantidade de espaços em branco
+        Console.WriteLine($"A frase contém {contagemEspacos} espaços em branco.");
+    }
+}
