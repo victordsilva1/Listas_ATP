5b95dad652b22d93d0e37792bf2c751d4e3afec1
Victor Silva <110117696+victordsilva1@users.noreply.github.com>
2024-07-01 15:21:47 -0300

    Update ex02.cs

--- ATP/lista06/ex02.cs
+++ ATP/lista06/ex02.cs
@@ -1,1 +1,25 @@
+using System;
 
+class RemoverVogais
+{
+    static void Main()
+    {
+        // Solicita ao usuário que insira uma frase
+        Console.WriteLine("Digite uma frase:");
+        string frase = Console.ReadLine();
+        
+        string resultado = "";
+
+        // Remove vogais da frase
+        foreach (char c in frase)
+        {
+            if (!"aeiouAEIOU".Contains(c))
+            {
+                resultado += c;
+            }
+        }
+
+        // Exibe a frase sem vogais
+        Console.WriteLine($"Frase sem vogais: {resultado}");
+    }
+}
