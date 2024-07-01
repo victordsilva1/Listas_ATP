cbab3b2e32c823b3a091d3bd425253bbba9adb71
Victor Silva <110117696+victordsilva1@users.noreply.github.com>
2024-07-01 15:27:54 -0300

    Update ex03.cs

--- ATP/lista06/ex03.cs
+++ ATP/lista06/ex03.cs
@@ -1,1 +1,38 @@
+using System;
+
+class CodigoCesar
+{
+    static void Main()
+    {
+        // Solicita ao usuário que insira uma string
+        Console.WriteLine("Digite uma string:");
+        string entrada = Console.ReadLine();
+
+        string resultado = "";
+
+        // Codifica cada caractere usando o Código de César (desloca 3 posições)
+        foreach (char c in entrada)
+        {
+            if (char.IsLetter(c))
+            {
+                char deslocado = (char)(c + 3);
+                
+                // Ajusta o deslocamento para dentro dos limites do alfabeto
+                if (char.IsLower(c) && deslocado > 'z' || char.IsUpper(c) && deslocado > 'Z')
+                {
+                    deslocado = (char)(deslocado - 26);
+                }
+
+                resultado += deslocado;
+            }
+            else
+            {
+                resultado += c;
+            }
+        }
+
+        // Exibe a string codificada em maiúsculas
+        Console.WriteLine($"String codificada: {resultado.ToUpper()}");
+    }
+}
 
